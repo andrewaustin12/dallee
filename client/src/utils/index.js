957db42e25b8524105ca4c7043cb5c4e39b6963d
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-03-21 18:38:35 -0400

    added create post page.

---
+++ client/src/utils/index.js
@@ -0,0 +1,12 @@
+import { surpriseMePrompts } from '../constants';
+
+export function getRandomPrompt(prompt) {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPromt = surpriseMePrompts[randomIndex];
+
+  if(randomPromt === prompt) {
+    return getRandomPrompt(prompt)
+  }
+
+  return randomPromt;
+}
